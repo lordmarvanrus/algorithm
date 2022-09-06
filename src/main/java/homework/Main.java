b52b82eae5a8b8523213cc7edc8ca742b781447c
Vladimir <marvalo73@gmail.com>
2022-09-06 19:23:06 +0700

    Готово

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -1,7 +1,21 @@
 package homework;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        StringList stringList = new StringListImpl();
+        stringList.add("test1");
+        stringList.add("test2");
+        stringList.add("test3");
+        stringList.add("test4");
+
+        stringList.remove(1);
+
+        System.out.println(Arrays.toString(stringList.toArray()));
+
+        stringList.clear();
+        System.out.println(stringList.size());
 
     }
 }
